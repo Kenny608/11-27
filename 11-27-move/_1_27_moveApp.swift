c50fcadba54d08e43377cfabddddfa6e027f1574
Kenny <kennypluchau@gmail.com>
2023-11-28 20:07:04 -0800

    inital

---
+++ 11-27-move/_1_27_moveApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_27_moveApp.swift
+//  11-27-move
+//
+//  Created by Kenny Luchau on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct _1_27_moveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
